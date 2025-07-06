abc2bac9fa7c63fd18c57678cb089c4268a260d0
slimebathtub <slimebathtub@gmail.com>
2025-07-06 16:31:52 -0700

    Solve Message Route

--- CSES/GRAPH/MessageRoute.cpp
+++ CSES/GRAPH/MessageRoute.cpp
@@ -3,40 +3,78 @@
 #include<queue>
 using namespace std;
 typedef long long ll;
-#define MAX 100000
+#define MAX 100005
 
-vector<int> graph;
-vector < vector<int> > parent;
-
-
-void dfs(int start, int end){
-
-}
+vector < vector<int> > graph;
+vector <int> parent;
 
 int main(){
     int point_num, road_num;
     cin >> point_num >> road_num;
-    parent.resize(point_num);
+    graph.resize(point_num+1);
+    parent.resize(point_num+1);
     for(int i = 0 ; i < road_num ; i++){
         int point_a, point_b;
         cin >> point_a >> point_b;
-        parent[point_a].push_back(point_b);
-        parent[point_b].push_back(point_a);
+        graph[point_a].push_back(point_b);
+        graph[point_b].push_back(point_a);
     }
     queue<int> next_road;
-    bool visted[10005] = {0};
+    bool visted[MAX] = {0};
     next_road.push(1);
+    bool end = false;
+
     while(!next_road.empty()){
         int current_point = next_road.front();
-        if(current_point == point_num) break;
-        for(int i = 0 ; i < parent[current_point].size(); i++){
-            if (!visted[parent[current_point][i]]){
-                next_road.push(parent[current_point][i]);
-                visted[parent[current_point][i]] = true;
+        //cout << "Round: " << current_point << endl;
+        next_road.pop();
+        vector<int> adj = graph[current_point];
+        //cout << "Vistied: " << current_point << endl;
+        visted[current_point] = true;
+        if(current_point == point_num) {
+            end = true;
+            break;
+        }
+        for(int i = 0 ; i < adj.size(); i++){
+            /*
+            if(adj[i] == point_num){
+                parent[adj[i]] = current_point;
+                end = true;
+                break;
+            }
+            */
+            if (!visted[adj[i]]){
+                next_road.push(adj[i]);
+                parent[adj[i]] = current_point;
+                visted[adj[i]] = true;
+                //cout << "adj[i]: " << adj[i] << endl;
             }
         }
-        next_road.pop();
+        if(end){
+            break;
+        }
     }
-    cout << << endl;
-
+    /*
+    cout << "Parent: " << endl;
+    for(int i = 1 ; i < parent.size(); i++){
+        cout << i << ": " << parent[i] << endl;
+    }
+    */
+    
+    if(end){
+        //cout << "Answer: " << endl;
+        int current = point_num;
+        vector<int> path;
+        while(current != 0){
+            path.push_back(current);
+            current = parent[current];
+        }
+        cout << path.size() << endl;
+        int end_num = path.size()-1; // last number = 0
+        for(int i = end_num; i >= 0; i--){
+            cout << path[i] << " ";
+        }
+    } else {
+        cout << "IMPOSSIBLE" << endl;
+    }
 }
